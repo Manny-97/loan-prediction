1b4bae80e39d6e1ed86040c72fe11c80a13568dd
Muhammed <muhammed@sendme.ng>
2023-01-17 11:00:18 +0100

    feat: add predict web services

---
+++ predict.py
@@ -0,0 +1,32 @@
+import pickle
+
+from flask import Flask, request, jsonify
+
+with open('model.bin', 'rb') as f_in:
+    (dv, model) = pickle.load(f_in)
+
+
+
+def predict(features):
+    pred = model.predict(features)
+    return int(pred)
+
+
+app = Flask('duration-prediction')
+
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
+    features = request.get_json()
+
+    pred = predict(features)
+
+    result = {
+        'loan_status': pred
+    }
+
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
